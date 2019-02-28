c2994d5bf2efca8685eed5b70f24cc885f16a55a
akm <akm2000@gmail.com>
2019-02-28 18:58:29 +0900

    :pencil2: Add comments into summarizer.go

--- summarizer.go
+++ summarizer.go
@@ -5,6 +5,7 @@ import (
 	"regexp"
 )
 
+// Replacer replaces string with something
 type Replacer interface {
 	Do(string) string
 }
@@ -15,23 +16,41 @@ func (r *replacerNullObject) Do(s string) string {
 	return s
 }
 
+// Replacement is a kind of Replacer.
+// Replaces a string which matches `Pattern` with `Replace`.
 type Replacement struct {
 	Pattern *regexp.Regexp
 	Replace string
 }
 
+// Do returns a string from given string `s`.
 func (r *Replacement) Do(s string) string {
 	return r.Pattern.ReplaceAllString(s, r.Replace)
 }
 
+// Summarizer counts for each `path` in data.
+// `path` means a route from the root to a node.
 type Summarizer struct {
-	Result         map[string]int
-	KeyCollapse    Replacer
+	// Result is a mamory to count up.
+	Result map[string]int
+
+	// KeyCollapse is a Replacer to summarize dynamic attribute names.
+	KeyCollapse Replacer
+
+	// RootExpression is used as root of `path`. default is ""
 	RootExpression string
-	PathSeparator  string
-	ArrayPrefix    string
-	ArraySuffix    string
-	TypeSeparator  string
+
+	// PathSeparator is separator between node names in `path`. default is "."
+	PathSeparator string
+
+	// ArrayPrefix is prefix for array object([]interface{}) in `path`. default is ""
+	ArrayPrefix string
+
+	// ArrayPrefix is suffix for array object([]interface{}) in `path`. default is "[]"
+	ArraySuffix string
+
+	// TypeSeparator is separator between `path` and object type. default is "$"
+	TypeSeparator string
 }
 
 func (s *Summarizer) Fulfill() {
@@ -55,6 +74,7 @@ func (s *Summarizer) Fulfill() {
 	}
 }
 
+// Load starts count up
 func (s *Summarizer) Load(obj interface{}) {
 	s.Fulfill()
 	s.Walk(s.RootExpression, obj)
