42243c34e4dbff3726e72323359620142a875939
akm <akm2000@gmail.com>
2019-02-28 15:32:55 +0900

    :shower: Remove debug messages

--- cmd/summarizejson/main.go
+++ cmd/summarizejson/main.go
@@ -35,14 +35,12 @@ func newSummarizer() *summarizejson.Summarizer {
 		Result: map[string]int{},
 	}
 
-	fmt.Fprintf(os.Stderr, "keypattern: %v\n", KeyPattern)
 	if KeyPattern != nil && *KeyPattern != "" {
 		ptn, err := regexp.Compile(*KeyPattern)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: Invalid regular expression keypattern: %s because of %v\n", *KeyPattern, err)
 			os.Exit(1)
 		}
-		fmt.Fprintf(os.Stderr, "keypattern: %s => %v\n", *KeyPattern, ptn)
 		s.KeyCollapse = &summarizejson.Replacement{
 			Pattern: ptn,
 			Replace: *KeyReplace,
@@ -73,10 +71,7 @@ func main() {
 
 	s := newSummarizer()
 
-	fmt.Fprintf(os.Stderr, "flag,args %v\n", flag.Args())
 	for _, path := range flag.Args() {
-		fmt.Fprintf(os.Stderr, "Loafing %s\n", path)
-
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "ERROR: Failed to read %s because of %v\n", path, err)
